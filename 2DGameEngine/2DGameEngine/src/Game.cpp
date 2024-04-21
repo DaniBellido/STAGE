e8be24554366644f4f7e9f1d5f894167f062bfee
Daniel Bellido <103462533+DaniBellido@users.noreply.github.com>
2024-04-21 23:45:38 +0100

    Few SDL events implemented
    Events to quit the application by closing the window or pressing ESC key.

--- 2DGameEngine/2DGameEngine/src/Game.cpp
+++ 2DGameEngine/2DGameEngine/src/Game.cpp
@@ -1,8 +1,16 @@
 #include "Game.h"
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+#include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include <sol/sol.hpp>
 #include <iostream>
 
 Game::Game() 
 {
+	isRunning = false;
 	std::cout << "Game Constructor called!" << std::endl;
 }
 
@@ -13,12 +21,51 @@ Game::~Game()
 
 void Game::Initialize() 
 {
+	// Initialize SDL libraries
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) 
+	{
+		// If init not possible output an error and return
+		std::cerr << "Error initializing SDL." << std::endl;
+		return;
+	}
 
+	// Creating a Window (SDL Struct pointer)
+	window = SDL_CreateWindow(
+		"STAGE: Simple Two-dimensional Animation Game Engine",    // Window Title
+		SDL_WINDOWPOS_CENTERED,					                  // Window X Position
+		SDL_WINDOWPOS_CENTERED,					                  // Window Y Position
+		800,									                  // Window Width	
+		600,									                  // Window Height
+		SDL_WINDOW_MAXIMIZED);					                  // Window Type
+
+	if (!window) 
+	{
+		// If window not created output an error and return
+		std::cerr << "Error creating the SDL window." << std::endl;
+		return;
+	}
+
+	// Create a renderer (SDL Struct pointer) on the created window (frame)
+	// second argument:  index of the rendering driver to use. -1 uses the default one
+	// third argument: allows you to specify additional options or flags for creating the renderer
+	renderer = SDL_CreateRenderer(window, -1, 0);
+
+	if (!renderer) 
+	{
+		// If renderer not created output an error and return
+		std::cerr << "Error creating the SDL renderer." << std::endl;
+		return;
+	}
+	
+	// Only true if creating window and renderer was successful 
+	isRunning = true;
+	
 }
 
 void Game::Run() 
 {
-	while (true) 
+	// Game Loop
+	while (isRunning) 
 	{
 		ProcessInput();
 		Update();
@@ -29,7 +76,29 @@ void Game::Run()
 
 void Game::ProcessInput() 
 {
+	// Struct Event
+	SDL_Event sdlEvent;
 
+	while (SDL_PollEvent(&sdlEvent))   // Passed as a reference to provide the memory address and not the whole struct
+	{
+		switch (sdlEvent.type) 
+		{
+			case SDL_QUIT:
+				// Event to quit the application by closing the window [X]
+				isRunning = false;
+				break;
+			case SDL_KEYDOWN:
+				// Event to quit the application by pressing ESC key
+				if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) 
+				{
+					// Trigger that stops the Run() method breaking the Game Loop and proceeds to call Destroy() in main.cpp
+					isRunning = false;
+				}
+				break;
+
+		
+		}
+	}
 }
 
 void Game::Update() 
@@ -44,5 +113,8 @@ void Game::Render()
 
 void Game::Destroy() 
 {
-
+	// Destroying renderer and window in the inverse order they were created
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
--- 2DGameEngine/2DGameEngine/src/Game.h
+++ 2DGameEngine/2DGameEngine/src/Game.h
@@ -1,12 +1,20 @@
 #pragma once
+#include <SDL.h>
+
 class Game
 {
 private:
+	// Global private variables (Attributes) to use in different member functions
+	bool isRunning;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
 
 public:
+	// Constructor/Destructor
 	Game();
 	~Game();
 
+	// Member Functions
 	void Initialize();
 	void Run();
 	void ProcessInput();

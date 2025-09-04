fd38a5aa4fda0a76564fefc916ce75d0eff88b76
Daniel Bellido <103462533+DaniBellido@users.noreply.github.com>
2025-09-05 00:43:36 +0200

    Keyboard Control System

--- 2DGameEngine/2DGameEngine/src/Game/Game.cpp
+++ 2DGameEngine/2DGameEngine/src/Game/Game.cpp
@@ -15,6 +15,7 @@
 #include "../Components/SpriteComponent.h"
 #include "../Components/AnimationComponent.h"
 #include "../Components/BoxColliderComponent.h"
+#include "../Components/KeyboardControlledComponent.h"
 #include "../Systems/MovementSystem.h"
 #include "../Systems/RenderSystem.h"
 #include "../Systems/AnimationSystem.h"
@@ -161,7 +162,7 @@ void Game::LoadLevel(int level)
 	assetStore->AddTexture(renderer, "tank-image", "./assets/images/tank-panther-right.png");
 	assetStore->AddTexture(renderer, "truck-image", "./assets/images/truck-ford-right.png");
 	assetStore->AddTexture(renderer, "tilemap-image", "./assets/tilemaps/jungle.png");
-	assetStore->AddTexture(renderer, "chopper-image", "./assets/images/chopper.png");
+	assetStore->AddTexture(renderer, "chopper-image", "./assets/images/chopper-spritesheet.png");
 	assetStore->AddTexture(renderer, "radar-image", "./assets/images/radar.png");
 
 	// Load the tilemap
@@ -199,6 +200,15 @@ void Game::LoadLevel(int level)
 	chopper.AddComponent<RigidBodyComponent>(glm::vec2(0.0, 0.0));
 	chopper.AddComponent<SpriteComponent>("chopper-image", 32, 32, 1);
 	chopper.AddComponent<AnimationComponent>(2, 10, true);
+	chopper.AddComponent<KeyboardControlledComponent>(glm::vec2(0, -20), glm::vec2(20, 0), glm::vec2(0, 20), glm::vec2(-20, 0));
+
+	Entity chopperB = registry->CreateEntity();
+	chopperB.AddComponent<TransformComponent>(glm::vec2(200.0, 100.0), glm::vec2(1.0, 1.0), 0.0);
+	chopperB.AddComponent<RigidBodyComponent>(glm::vec2(0.0, 0.0));
+	chopperB.AddComponent<SpriteComponent>("chopper-image", 32, 32, 1);
+	chopperB.AddComponent<AnimationComponent>(2, 10, true);
+	chopperB.AddComponent<KeyboardControlledComponent>(glm::vec2(0, -50), glm::vec2(50, 0), glm::vec2(0, 50), glm::vec2(-50, 0));
+	
 
 	Entity radar = registry->CreateEntity();
 	radar.AddComponent<TransformComponent>(glm::vec2(windowWidth - 74, 10.0), glm::vec2(1.0, 1.0), 0.0);

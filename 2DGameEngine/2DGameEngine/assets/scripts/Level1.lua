5da524f9c71108f9b0b21cf3513870711a29ad86
Daniel Bellido <103462533+DaniBellido@users.noreply.github.com>
2025-10-04 19:31:59 +0200

    ScriptSystem test successful
    The program still runs

--- 2DGameEngine/2DGameEngine/assets/scripts/Level1.lua
+++ 2DGameEngine/2DGameEngine/assets/scripts/Level1.lua
@@ -2755,7 +2755,7 @@ Level = {
                 }
             }
         },
-        --[[
+        
         {
             -- SU-27 fighter jet
             group = "enemies",
@@ -2795,9 +2795,9 @@ Level = {
                 on_update_script = {
                     [0] =
                     function(entity, delta_time, ellapsed_time)
-                        -- print("Executing the SU-27 fighter jet Lua script!")
+                        print("Executing the SU-27 fighter jet Lua script!")
 
-                        -- this function makes the fighter jet move up and down the map shooting projectiles
+                    --[[  -- this function makes the fighter jet move up and down the map shooting projectiles
                         local current_position_x, current_position_y = get_position(entity)
                         local current_velocity_x, current_velocity_y = get_velocity(entity)
 
@@ -2815,7 +2815,7 @@ Level = {
                         else
                             set_rotation(entity, 180) -- point down
                             set_projectile_velocity(entity, 0, 200) -- shoot projectiles down
-                        end
+                        end--]]
                     end
                 }
             }
@@ -2859,17 +2859,17 @@ Level = {
                 on_update_script = {
                     [0] =
                     function(entity, delta_time, ellapsed_time)
-                        -- print("Executing BF-109 Lua script!")
+                        print("Executing BF-109 Lua script!")
 
-                        -- change the position of the the airplane to follow a sine wave movement
+                    --[[    -- change the position of the the airplane to follow a sine wave movement
                         local new_x = ellapsed_time * 0.09
                         local new_y = 200 + (math.sin(ellapsed_time * 0.001) * 50)
-                        set_position(entity, new_x, new_y) -- set the new position
+                        set_position(entity, new_x, new_y) -- set the new position --]]
                     end
                 }
             }
         }
-        --]]
+       
     }
 }
 
